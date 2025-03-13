f9ee5b8d5cb120d7649824419025fa564d80a45d
Islamov Elkhan <elkhanmi@code.local>
2025-03-13 20:33:32 +0400

    Category

---
+++ DemoProject.Application/Repostories/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using DemoProject.Domain.Entities;
+
+namespace DemoProject.Application.Repostories;
+
+public interface ICategoryRepository
+{
+    List<Category> GetCategories();
+    void AddCategory(Category category);
+    void RemoveCategory(int id);
+    void UpdateCategory(int id, Ctagory product);
+}
--- DemoProject.Domain/Entities/Category.cs
+++ DemoProject.Domain/Entities/Category.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace DemoProject.Domain.Entities
 {
-    class Category
+    public class Category:Entity
     {
+        public string Name { get; set; }
     }
 }
